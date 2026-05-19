9c669a0232ce2f5f6fa3fe7484bb3436a946e21a
Squeng <paul@squeng.com>
2026-05-19 20:18:13 +0200

    moved to hexagon

--- Deposplit/driven_ports/ShareRepository.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-protocol ShareRepository {
-    func getAll() -> [HeldShare]
-    func getCiphertext(shareId: UUID) -> Data?
-    func save(_ share: HeldShare)
-    func delete(shareId: UUID)
-}
